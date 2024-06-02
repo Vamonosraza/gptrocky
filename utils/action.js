1584978fd88915ab1792b6ffe488e308fb57c592
Vamonosraza <martinezjessy393@gmail.com>
2024-06-02 15:25:49 -0700

    Working on Chat Bot

--- utils/action.js
+++ utils/action.js
@@ -1,6 +1,21 @@
 'use server'
+import OpenAI from "openai";
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
 
 export const generateChatResponse = async (message) => {
-    console.log(message);
+    const response = await openai.chat.completions.create({
+        messages:[
+            {role:'system', content:'You are a helpful assistant. limit to two sentences'},
+            {role:'user', content:message}
+        ],
+        model:"gpt-3.5-turbo-0613",
+        temperature: 1,
+    });
+    console.log(response.choices[0].message);
+    console.log(response)
     return 'awesome message'
 }
