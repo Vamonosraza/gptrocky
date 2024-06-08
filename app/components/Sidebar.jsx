5fb9dc58dcdaddf7abd9abdf19f23a1590ab49f9
Vamonosraza <martinezjessy393@gmail.com>
2024-06-08 15:45:25 -0700

    Trying to solve deployment problem

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import MemberProfile from './MemberProfile';
 import NavLinks from './NavLinks';
-import SidebarHeader from './SidebarHeader';
+import SidebarHeader from './SidebarHeaderTemp';
 
 const Sidebar = () => {
   return (
     <div className='px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]'>
       {/* first row */}
       <SidebarHeader />
-      
+
       {/* second row */}
       <NavLinks />
       {/* third row */}
