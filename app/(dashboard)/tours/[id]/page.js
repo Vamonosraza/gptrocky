746723aa45c896b29c19559de5b8f7bd83b678a7
Vamonosraza <martinezjessy393@gmail.com>
2024-06-07 20:16:34 -0700

    Tour images are viewable using unsplash API

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -3,6 +3,9 @@ import { getSingleTour } from '@/utils/action'
 import React from 'react'
 import Link from 'next/link'
 import { redirect} from 'next/navigation'
+import axios from 'axios'
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`;
+import Image from 'next/image'
 
 const SingleTOurPage = async ({params}) => {
     const tour = await getSingleTour(params.id)
@@ -10,9 +13,23 @@ const SingleTOurPage = async ({params}) => {
     if(!tour){
         redirect('/tours')
     }
-  return (
+    const {data} = await axios.get(`${url}${tour.city}`);
+    const tourImage = data?.results[0]?.urls?.raw
+    return (
     <div>
         <Link href='/tours' className='btn btn-secondary mb-12'>Back to tours</Link>
+        {tourImage? (
+            <div>
+                <Image
+                    src={tourImage}
+                    alt={tour.city}
+                    layout='responsive'
+                    width={1000}
+                    height={600}
+                    className='rounded-xl'
+                    />
+            </div>
+        ): null}
         <TourInfo tour={tour} />
     </div>
   )
