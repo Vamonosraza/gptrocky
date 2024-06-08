dc79c91819ebcc2c7e990b59ade4c74fefa339ab
Vamonosraza <martinezjessy393@gmail.com>
2024-06-08 15:37:24 -0700

    trying

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -7,6 +7,7 @@ const Sidebar = () => {
     <div className='px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]'>
       {/* first row */}
       <SidebarHeader />
+      
       {/* second row */}
       <NavLinks />
       {/* third row */}
